1d794b7d67edea0a48dcf46d79b1bc00f79512ba
Hank Goddard <me@henrygoddard.com>
2018-07-25 20:11:44 -0400

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# Blackbook
+
+Built off of the [grand-stack-starter](https://github.com/grand-stack/grand-stack-starter) repo, a CRM organized by relationships.
+
+## Quickstart
+
+*Install dependencies*
+
+```
+(cd ./ui && npm install)
+(cd ./api && npm install)
+```
+
+*Start API server*
+```
+cd ./api && npm start
+```
+
+This will start the GraphQL API in the foreground, so in another terminal session start the UI development server:
+
+*Start UI server*
+```
+cd ./ui && npm start
+```
+
+## [`/api`](./api)
+![](api/img/graphql-playground.png)
+
+## [`/ui`](./ui)
+![](ui/img/default-app.png)
---
+++ ui/src/UserList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import "./UserList.css";
+
+const UserList = () => (
+  <Query
+    query={gql`
+      {
+        users(first: 10) {
+          name
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      console.log("DATA: ", data);
+      console.log("Error: ", error);
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error</p>;
+
+      return (
+        <div className="UserList">
+          <h1>Users:</h1>
+          <ul>{data.users.map(({ name }, i) => <li key={i}>{name}</li>)}</ul>
+        </div>
+      );
+    }}
+  </Query>
+);
+
+export default UserList;
